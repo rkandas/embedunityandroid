33cf5f57c71f786577e3f78a0ee849ce73693aac
rkandaswamy <rkandaswamy@lenovo.com>
2020-02-20 14:31:52 +0530

    missing files

---
+++ Assets/DataExchanger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DataExchanger : MonoBehaviour
+{
+    public InputField fld;
+
+    // a method which can be called from Android app
+    public void ShowMessage(string message)
+    {
+        fld.text = message;
+    }
+
+    public void PassDataToAndroid()
+    {
+        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+        activity.CallStatic("setDataFromUnity", new object[] {fld.text});
+    }
+}
